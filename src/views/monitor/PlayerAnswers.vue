4b0d1d9f06383008e1feddd4e8a4437a651108b6
Shyne <yeshai007@gmail.com>
2022-10-23 23:54:18 +0800

    Update PlayerAnswers.vue

--- src/views/monitor/PlayerAnswers.vue
+++ src/views/monitor/PlayerAnswers.vue
@@ -1,20 +1,21 @@
 <template>
   <v-list class="d-flex flex-column flex-start fill-height py-0" style="background: black;">
+    <v-divider style="border-width: thick; border-color: black;"></v-divider>
     <div v-for="answer in answers" :key="answer.attendance_id">
       <template>
         <v-list-item class="px-3 white">
           <v-list-item-content
             class="py-0 font-weight-bold"
-            style="font-size: 5vh; line-height: 1;">
+            style="font-size: 5vh; line-height: 1; width: 20%;">
             {{ getPlayerName(answer.attendance_id) }}
           </v-list-item-content>
           <v-list-item-action
             class="align-center my-1 mr-0 justify-end"
-            style="font-size: 5vh; line-height: 1;">
+            style="font-size: 5vh; line-height: 1; width: 80%; justify-content: flex-start !important;">
             {{ answer.answer }}
           </v-list-item-action>
         </v-list-item>
-        <v-divider style="border-width: medium; border-color: black;"></v-divider>
+        <v-divider style="border-width: thick; border-color: black;"></v-divider>
       </template>
     </div>
   </v-list>
