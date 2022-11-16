b914343aea2b2ef6b264af02cac9fdab5aa10dea
Shyne <yeshai007@gmail.com>
2022-11-16 09:18:25 +0800

    Update Games.vue

--- src/views/list/Games.vue
+++ src/views/list/Games.vue
@@ -68,7 +68,6 @@
 
 <script>
 import moment from 'moment'
-import apiGames from '@/api/games'
 
 export default {
   name: 'view-games-list',
@@ -107,7 +106,7 @@ export default {
   methods: {
     getGames: function () {
       this.loadingItems = true
-      apiGames.getAll()
+      this.$api.games.getAll()
         .then(response => {
           this.games = response.games
         }).catch(err => {
@@ -140,18 +139,7 @@ export default {
     saveGame: async function () {
       this.form.submitting = true
       try {
-        var response = {}
-        if (this.form.data.id > 0) {
-          response = await apiGames.update({
-            game_id: this.form.data.id,
-            game: {
-              title: this.form.data.title,
-              date: this.form.data.date
-            }
-          })
-        } else {
-          response = await apiGames.create(this.form.data)
-        }
+        var response = await this.$api.games.update(this.form.data)
         this.$store.commit('SHOW_SNACKBAR', {
           status: 'success',
           message: response.message
