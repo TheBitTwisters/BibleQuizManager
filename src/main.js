9c2f0ebba03788cb2a661ff5fe6068eb951b015f
Shyne <yeshai007@gmail.com>
2022-11-19 14:21:21 +0800

    Revert "added api factory"
    This reverts commit 0e7d2bddd039c9d4c0819601406f0601c085856e.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-import App from '@/App.vue'
-import router from '@/router'
-import store from '@/store'
-import vuetify from '@/plugins/vuetify'
-
-import '@/api'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
 
 import '@/filters/datetime'
 import '@/filters/ordinal'
